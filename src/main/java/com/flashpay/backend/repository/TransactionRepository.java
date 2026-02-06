5ad8e5210f156f64062cd586d477a1b54e859b21
joaonascimentooo <joaonascimento197@gmail.com>
2026-02-05 22:21:29 -0300

    fix: add anottation for TransactionRepository

--- src/main/java/com/flashpay/backend/repository/TransactionRepository.java
+++ src/main/java/com/flashpay/backend/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.flashpay.backend.repository;
 
 import com.flashpay.backend.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
 }
