a0bbe825a9b3c6da44f7edcd0f4b23e2c26afd48
joaonascimentooo <joaonascimento197@gmail.com>
2026-02-09 21:11:30 -0300

    feat: add logic in TransactionService

--- src/main/java/com/flashpay/backend/service/UserService.java
+++ src/main/java/com/flashpay/backend/service/UserService.java
@@ -2,24 +2,25 @@ package com.flashpay.backend.service;
 
 import com.flashpay.backend.domain.User;
 import com.flashpay.backend.enums.UserType;
+import com.flashpay.backend.repository.TransactionRepository;
 import com.flashpay.backend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public void validateTransaction(User sender, BigDecimal amount) throws Exception{
         if (sender.getUserType() == UserType.SHOPKEEPER){
-            throw new Exception("Lojistas não podem realizar transferencias");
+            throw new Exception("Lojistas não podem realizar transferências");
         }
 
-        if (sender.getBalance().compareTo(amount) <0 ){
+        if (sender.getBalance().compareTo(amount) < 0 ){
             throw new Exception("Saldo Insuficiente");
         }
     }
