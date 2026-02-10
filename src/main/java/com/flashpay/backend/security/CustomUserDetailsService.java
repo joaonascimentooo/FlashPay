0aaa5d973f15c98c7e0629299602685cb085d1ce
joaonascimentooo <joaonascimento197@gmail.com>
2026-02-10 02:57:17 -0300

    refactor: remove unnecessary blank line in CustomUserDetailsService

--- src/main/java/com/flashpay/backend/security/CustomUserDetailsService.java
+++ src/main/java/com/flashpay/backend/security/CustomUserDetailsService.java
@@ -21,7 +21,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-   
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
