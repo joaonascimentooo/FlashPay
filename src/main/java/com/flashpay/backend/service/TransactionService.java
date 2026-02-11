ccfa62a9fcf8823ac2f156c8814c9e356f7e0a45
joaonascimentooo <joaonascimento197@gmail.com>
2026-02-11 00:37:08 -0300

    fix: remove unnecessary blank lines in multiple service and security classes

--- src/main/java/com/flashpay/backend/service/TransactionService.java
+++ src/main/java/com/flashpay/backend/service/TransactionService.java
@@ -9,7 +9,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 
 @Service
